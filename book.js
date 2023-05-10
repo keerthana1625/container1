1a05891946aa05ce7611165c1a9a9903c9e2cfe0
keerthana1625 <65592959+keerthana1625@users.noreply.github.com>
2023-05-10 20:35:21 +0530

    Add files via upload

---
+++ book.js
@@ -0,0 +1,151 @@
+
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
+const seatContainer = document.querySelector(".row-container");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const movieSelect = document.getElementById("movie");
+
+populateUI();
+
+let ticketPrice = +movieSelect.value;
+
+// Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+}
+
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll(".container .selected");
+
+  seatsIndex = [...selectedSeats].map(function(seat) {
+    return [...seats].indexOf(seat);
+  });
+
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
+  let selectedSeatsCount = selectedSeats.length;
+  count.textContent = selectedSeatsCount;
+  total.textContent = selectedSeatsCount * ticketPrice;
+}
+
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach(function(seat, index) {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
+// Movie select event
+
+movieSelect.addEventListener("change", function(e) {
+  ticketPrice = +movieSelect.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
+  updateSelectedCount();
+});
+
+// Adding selected class to only non-occupied seats on 'click'
+
+seatContainer.addEventListener("click", function(e) {
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied")
+  ) {
+    e.target.classList.toggle("selected");
+    updateSelectedCount();
+  }
+});
+
+// Initial count and total rendering
+updateSelectedCount();
+
+
+  // Get the current date and time
+
+  function validatebooking()
+  {
+    var val= document.getElementById('movie').value;
+    var m1="150";
+    var m2="250";
+    var m3="120";
+    var m4="750";
+    const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+    if (val===m1 || val===m2 || val===m3 || val===m4) 
+    {
+      if(selectedSeats !== null && selectedSeats.length > 0)
+      {
+        alert("Booking Successful");
+        return true;
+      }
+      else
+      {
+        alert("Please select atleast one seat");
+        return false;
+      }
+    }
+    else if(count==0)
+    {
+      alert("Please select atleast one seat");
+      return false;
+    }
+    else
+    {
+      alert("Movie is not been selected");
+      return false;
+    }
+  }
+
+  const now = new Date();
+
+  // Set the time of the next show (replace with your own show time)
+  const showTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 19, 30, 0); // 7:30 PM
+  
+  // Calculate the time difference between now and the show time
+  const timeDifference = showTime - now;
+  
+  // Convert the time difference to seconds
+  let remainingTime = Math.floor(timeDifference / 1000);
+  
+  // Function to update the timer display
+  function updateTimer() {
+    // Calculate the hours, minutes, and seconds
+    const hours = Math.floor(remainingTime / 3600);
+    const minutes = Math.floor((remainingTime % 3600) / 60);
+    const seconds = remainingTime % 60;
+  
+    // Display the remaining time on the page
+    timer.textContent = `${hours.toString().padStart(2, "0")}:${minutes
+      .toString()
+      .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+  
+    // Decrease the remaining time by 1 second
+    remainingTime--;
+  
+    // Check if the timer has reached zero
+    if (remainingTime < 0) {
+      clearInterval(timerInterval); // Stop the timer
+      timer.textContent = "Showtime!"; // Display a message when the show starts
+    }
+  }
+  
+  // Initial call to update the timer display
+  updateTimer();
+  
+  // Update the timer every second
+  const timerInterval = setInterval(updateTimer, 1000);
+
+  
+
+    
+  
